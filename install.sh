2b0c00c9574b2e78cc03704efbba4d1a90e2aa0f
qiuxianchao <qiuxianchao@wtoip.com>
2020-04-17 17:44:14 +0800

    add

---
+++ install.sh
@@ -0,0 +1,61 @@
+#!/data/data/com.termux/files/usr/bin/sh
+
+print_status() {
+    printf "[*] ${1}...\n"
+}
+
+get_tar() {
+    wget -c https://github.com/Hax4us/java/releases/download/v8/jdk8_aarch64.tar.gz -O jdk8_aarch64.tar.gz
+    tar -xf jdk8_$ARCH.tar.gz -C $PREFIX/share
+    chmod +x $PREFIX/share/bin/*
+    mv $PREFIX/share/bin/* $PREFIX/bin
+}
+
+cleanup() {
+    rm -f jdk8_aarch64.tar.gz
+    rm -rf $PREFIX/share/bin
+}
+
+prepare(){
+  termux-wake-lock
+  pkg install proot
+  termux-chroot
+}
+prepare_mysql(){
+  pkg install mariadb
+  cd $home
+  mysql_install_db
+  nohup mysqld &
+  echo -e '\n'
+}
+prepare_asset_sql(){
+  wget -c https://github.com/peterQiu/download/database.sql -O database.sql
+  wget -c https://github.com/peterQiu/download/schema.sql -O schema.sql
+  wget -c https://github.com/peterQiu/download/data.sql -O data.sql
+  sleep 5
+  mysql < ./database.sql
+  mysql < ./schema.sql
+  mysql < ./data.sql
+}
+
+prepare_asset_jar(){
+  wget -c https://github.com/peterQiu/download/asset.jar -O asset.jar
+  nohup java -jar asset.jar &
+  echo -e '\n'
+}
+
+print_status "prepare"
+prepare
+print_status "getting tar file and setting all things"
+get_tar
+print_status "cleaning up"
+cleanup
+print_status "installing mysql"
+prepare_mysql
+print_status "preparing asset sql"
+prepare_asset_sql
+print_status "preparing asset jar"
+prepare_asset_jar
+print_status "finished"
+
+
